cdcb61add03707c78b0af3f0128cf2b12658e8bb
Juan Avila <dev@juanavila.es>
2020-10-11 12:23:53 +0200

    Split combination proposal logic

--- src/main/java/juanavila/mastermind/HumanBreakerPlayer.java
+++ src/main/java/juanavila/mastermind/HumanBreakerPlayer.java
@@ -1,7 +1,5 @@
 package juanavila.mastermind;
 
-import juanavila.mastermind.utils.Console;
-
 public class HumanBreakerPlayer implements Player {
 
     private Board board;
@@ -11,34 +9,8 @@ public class HumanBreakerPlayer implements Player {
     }
 
     public void play() {
-        String colors = Console.instance().readString(Message.COMBINATION_PROPOSAL.toString());
-        Color[] chosenColors;
-        do {
-            chosenColors = parseColors(colors);
-        }
-        while (chosenColors.length != Combination.ACCEPTED_LENGTH);
-
-        ProposedCombination combination = new ProposedCombination(chosenColors);
-        board.proposeCombination(combination);
-    }
-
-    private Color[] parseColors(String colors) {
-        Color[] position = new Color[Combination.ACCEPTED_LENGTH];
-        int i = 0;
-
-        Color chosen = Color.from(colors.charAt(i));
-        while (i < Combination.ACCEPTED_LENGTH && !chosen.isNull()) {
-            position[i] = chosen;
-            chosen = Color.from(colors.charAt(i));
-            i++;
-        }
-
-        if (chosen.isNull()) {
-            position = new Color[0];
-            Console.instance().writelnError(Error.WRONG_COLORS.toString(),Color.palette());
-        }
-
-        return position;
+        ProposedCombination chosenCombination = new CombinationPickerDialog().ask();
+        board.proposeCombination(chosenCombination);
     }
 
     public void announceWin() {
