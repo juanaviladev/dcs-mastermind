c48fc8bc6e1dc58a293a431dda5be7c1acb7e476
Juan Avila <dev@juanavila.es>
2020-10-12 12:51:23 +0200

    Undo dialog splitting

--- src/main/java/juanavila/mastermind/HumanBreakerPlayer.java
+++ src/main/java/juanavila/mastermind/HumanBreakerPlayer.java
@@ -3,10 +3,12 @@ package juanavila.mastermind;
 public class HumanBreakerPlayer implements Player {
 
     private Board board;
+    private static final Color[] NULL_ARRAY = new Color[0];
 
     public HumanBreakerPlayer(Board board) {
         this.board = board;
     }
+
 
     public void play() {
         ProposedCombination chosenCombination = new CombinationPickerDialog().ask();
