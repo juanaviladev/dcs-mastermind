ac6a2c80b27f42f8a7186f05b31aa0bc540a78a0
Juan Avila <dev@juanavila.es>
2020-10-12 12:51:55 +0200

    Undo dialog splitting

--- src/main/java/juanavila/mastermind/HumanBreakerPlayer.java
+++ src/main/java/juanavila/mastermind/HumanBreakerPlayer.java
@@ -1,5 +1,7 @@
 package juanavila.mastermind;
 
+import juanavila.mastermind.utils.Console;
+
 public class HumanBreakerPlayer implements Player {
 
     private Board board;
@@ -11,8 +13,39 @@ public class HumanBreakerPlayer implements Player {
 
 
     public void play() {
-        ProposedCombination chosenCombination = new CombinationPickerDialog().ask();
-        board.proposeCombination(chosenCombination);
+        Color[] chosenColors;
+        do {
+            String colors = Console.instance().readString(Message.COMBINATION_PROPOSAL.toString());
+            chosenColors = parseColors(colors);
+        }
+        while (chosenColors.length != Combination.ACCEPTED_LENGTH);
+
+        ProposedCombination combination = new ProposedCombination(chosenColors);
+        board.proposeCombination(combination);
+    }
+
+    private Color[] parseColors(String colors) {
+        if(colors.length() != Combination.ACCEPTED_LENGTH) {
+            Error.WRONG_PROPOSAL_LENGTH.writeln();
+            return NULL_ARRAY;
+        }
+
+        Color[] chosenColors = new Color[Combination.ACCEPTED_LENGTH];
+        int i = 0;
+
+        Color chosen = Color.from(colors.charAt(i));
+        while (i < Combination.ACCEPTED_LENGTH && !chosen.isNull()) {
+            chosenColors[i] = chosen;
+            chosen = Color.from(colors.charAt(i));
+            i++;
+        }
+
+        if (chosen.isNull()) {
+            chosenColors = NULL_ARRAY;
+            Error.WRONG_COLORS.writeln(Color.palette());
+        }
+
+        return chosenColors;
     }
 
     public void announceWin() {
