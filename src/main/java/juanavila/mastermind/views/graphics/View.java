5fe7f7bc062307296e392e166ebc8746dc0c5229
Juan Avila <dev@juanavila.es>
2020-10-24 18:36:35 +0200

    Update with AcceptorController

--- src/main/java/juanavila/mastermind/views/graphics/View.java
+++ src/main/java/juanavila/mastermind/views/graphics/View.java
@@ -1,12 +1,12 @@
 package juanavila.mastermind.views.graphics;
 
-import juanavila.mastermind.controllers.Controller;
+import juanavila.mastermind.controllers.AcceptorController;
 import juanavila.mastermind.views.MastermindView;
 
 public class View extends MastermindView {
 
     @Override
-    public void interact(Controller controller) {
+    public void interact(AcceptorController controller) {
 
     }
 }
