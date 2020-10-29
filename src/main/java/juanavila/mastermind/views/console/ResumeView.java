6a6dc1017e9066d981aaf10428d119d2db8df14a
Juan Avila <dev@juanavila.es>
2020-10-29 20:23:27 +0100

    Fix bad rename

--- src/main/java/juanavila/mastermind/views/console/ResumeView.java
+++ src/main/java/juanavila/mastermind/views/console/ResumeView.java
@@ -1,11 +1,11 @@
 package juanavila.mastermind.views.console;
 
-import juanavila.mastermind.controllers.implementation.ResumeControllerImplementation;
+import juanavila.mastermind.controllers.ResumeController;
 import juanavila.utils.YesNoDialog;
 
 class ResumeView {
 
-	void interact(ResumeControllerImplementation controller) {
+	void interact(ResumeController controller) {
 		MessageView.RESUME.write();
 		boolean newGame = new YesNoDialog().read();
 		controller.resume(newGame);
